777b0e14d8509f2916eca908d465cff83bb8cc4a
mangal bodele <mangalbodele123@example.com>
2024-12-07 15:02:11 +0530

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,50 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-        <nav>
-            <ul>
-                <li><NavLink to="/Add">Add User</NavLink></li>
-                <li><NavLink to="/show">Show Users</NavLink></li>
-                <li><NavLink  to="/update">update User</NavLink></li>
-                <li><NavLink  to="/delete">delete User</NavLink></li>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/add">
+                  add user
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/show">
+                  Show users
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              
+            
             </ul>
-        </nav>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
